966b284700b9e32ac5d9ff7ce18bbd034dcf24c7
Kendall Wahnschaffe <kpwahnschaffe@gmail.com>
2017-05-02 17:36:04 -0500

    minor text fixes

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,3 +1,3 @@
 module.exports = function (req, res) {
-    res.json({ message: 'Catch all' });
+    res.json({ message: 'Catch all. Everything that comes here didnt find a real endpoint :(' });
 };
