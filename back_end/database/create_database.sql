a8e2f638a7cc310327349796144513f9177d4f32
Kendall Wahnschaffe <kpwahnschaffe@gmail.com>
2017-05-02 12:01:35 -0500

    Adding mysql to project

---
+++ back_end/database/create_database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS open_source_dance;
+
+CREATE TABLE user_table (
+    email varchar(255) NOT NULL
+);
--- back_end/server.js
+++ back_end/server.js
@@ -10,8 +10,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/* ROUTES */
 app.use('/users', require('./routes/users.js'));
-
 app.use(require('./routes/index.js'));
 
 app.listen(port, function() {
