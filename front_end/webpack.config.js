e6977b7c254ea5887ea4f87b1935cf68cd056ebe
Kendall Wahnschaffe <kpwahnschaffe@gmail.com>
2017-05-03 11:12:24 -0500

    Simplified webpack and added new image

--- front_end/webpack.config.js
+++ front_end/webpack.config.js
@@ -1,40 +1,32 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
     },
-    {
-      test: /\.css$/,
-      loader: 'style-loader'
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015', 'stage-1']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
     },
-    {
-      test: /\.css$/,
-      loader: 'css-loader',
-      query: {
-        modules: false,
-        localIdentName: '[name]__[local]___[hash:base64:5]'
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
     }
-}
-  ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
 };
